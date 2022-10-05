b4b6cfbdc6b5348e5f11620655bc567dd70fc855
Max Nerius <nermax03@gmail.com>
2022-10-05 19:33:37 +0200

    move tokenizer tests to the tokenizer module file

--- src/main.rs
+++ src/main.rs
@@ -9,30 +9,3 @@ fn main() -> Result<()> {
 
     Ok(())
 }
-
-#[cfg(test)]
-mod tests {
-    use crate::tokenizer::{self, Token};
-
-    #[test]
-    fn tokenization() {
-        use tokenizer::Token;
-
-        let input = "(henlo '(x 32 32.32))".to_string();
-        let tokens = tokenizer::tokenize(input.lines());
-        assert_eq!(
-            tokens,
-            vec![
-                Token::LParen,
-                Token::Symbol("henlo".to_string()),
-                Token::Quote,
-                Token::LParen,
-                Token::Symbol("x".to_string()),
-                Token::Integer(32i32),
-                Token::Float(32.32f32),
-                Token::RParen,
-                Token::RParen
-            ]
-        );
-    }
-}
